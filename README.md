05be5d6429225be4223293c0b643a7f593cd706e
xNoJustice <harunmimarsinan@gmail.com>
2023-08-01 15:39:11 +0300

    finished

---
+++ README.md
@@ -0,0 +1,7 @@
+## Installation
+
+1. `cp .env.sample .env`
+2. Add env variables to .env
+3. yarn
+4. yarn dev # For Dev
+5. yarn start # For Prod.
---
+++ index.js
@@ -0,0 +1,61 @@
+const {
+  REST,
+  Routes,
+  Client,
+  GatewayIntentBits,
+  SlashCommandBuilder,
+  PermissionFlagsBits,
+} = require("discord.js");
+require("dotenv").config();
+const commands = [
+  new SlashCommandBuilder()
+    .setName("unbanall")
+    .setDescription("Unbans all users from the server")
+    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+];
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.on("ready", async () => {
+  client.application.commands.set(commands);
+  console.log(
+    "Your bot invite:",
+    client.generateInvite({
+      scopes: ["bot", "applications.commands"],
+      permissions: [
+        PermissionFlagsBits.BanMembers,
+        PermissionFlagsBits.KickMembers,
+        PermissionFlagsBits.SendMessages,
+      ],
+    })
+  );
+  console.log("Use /unbanall to unban all users");
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+  if (interaction.commandName === "unbanall") {
+    const user = interaction.guild.members.cache.get(interaction.member.id);
+    const guild = interaction.guild;
+    if (user.permissions.has([PermissionFlagsBits.BanMembers])) {
+      try {
+        await interaction.reply("Starting unban...");
+        const bans = await rest.get(Routes.guildBans(guild.id));
+        const banNumbers = bans.length;
+        await interaction.editReply(
+          `Found ${banNumbers} bans in ${guild.name}`
+        );
+        for (const v of bans) {
+          await interaction.guild.members.unban(v.user.id);
+        }
+        await interaction.editReply(`Unbanned all ${banNumbers} users`);
+      } catch (e) {
+        await interaction.editReply(`There was some unexpected error:\n\n${e}`);
+      }
+    } else {
+      await interaction.reply("You do not have permission to use this command");
+    }
+  }
+});
+
+client.login(process.env.TOKEN);
